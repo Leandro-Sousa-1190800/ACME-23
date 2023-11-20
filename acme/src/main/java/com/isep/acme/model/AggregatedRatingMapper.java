479528099a980ffea07e43904b0997cda30f4a5e
Leandro <1190800@isep.ipp.pt>
2023-11-20 23:06:17 +0000

    Adds initial commit

--- acme/src/main/java/com/isep/acme/model/AggregatedRatingMapper.java
+++
@@ -1,9 +0,0 @@
-package com.isep.acme.model;
-
-public class AggregatedRatingMapper {
-
-    public static AggregatedRatingDTO toDto(AggregatedRating aggregatedRating){
-        return new AggregatedRatingDTO(aggregatedRating.getAggregatedId(),aggregatedRating.getAggregatedId(),ProductMapper.toDto(aggregatedRating.getProduct()));
-    }
-
-}
--- acme/src/main/java/com/isep/acme/model/ProductMapper.java
+++
@@ -1,9 +0,0 @@
-package com.isep.acme.model;
-
-public class ProductMapper {
-
-    public static ProductDTO toDto(Product product){
-        return new ProductDTO(product.getSku(),product.getDesignation());
-    }
-
-}
